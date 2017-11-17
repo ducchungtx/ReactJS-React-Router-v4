9b1e505fbcad5f2505c9360b36123b375eeb3904
Chung Nguyen <zrmedia9@gmail.com>
2017-11-17 10:53:17 +0700

    Promt when change stage (Xuất thông báo tương tự FB nếu thoát trình chat đng có nội dung)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { Prompt } from 'react-router-dom';
 
 class Contact extends Component {
-    state = {  }
+    state = { 
+        isChecked: false
+    }
+    onClick(check) {
+        this.setState({
+            isChecked: check
+        })
+    }
     render() {
         return (
-            <div>CONTACT</div>
+            <div>
+                CONTACT<br/>
+                
+                <button onClick={() => this.onClick(false)} type="button" className="btn btn-info">Cho phép</button><br/><br/>
+                <button onClick={() => this.onClick(true)} type="button" className="btn btn-danger">Không Cho phép</button><br/>
+                <Prompt
+                    when={this.state.isChecked}
+                    message={ location => (`Bạn muốn chắc muốn đến trang ${location.pathname}`) }
+                />
+            </div>
         );
     }
 }
